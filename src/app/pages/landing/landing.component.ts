4dd94bc9010580c0d4b405eb9677cdfdcc36278c
codecraftwt <codecraftwt@gmail.com>
2025-04-24 09:36:54 +0530

    landing component updated

--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -13,4 +13,14 @@ import { SearchComponent } from '../../shared/components/search/search.component
 })
 export class LandingComponent {
 
+  cardsData = [
+    { id: 1, title: 'Card Placeholder', image: '../../../../assets/images/Hand_Knitted_Socks.jpg' },
+    { id: 2, title: 'Card Placeholder', image: '../../../../assets/images/Shea_Butter_Body_Cream.jpg' },
+    { id: 3, title: 'Card Placeholder', image: '../../../../assets/images/Hand_Knitted_Socks.jpg' },
+    { id: 4, title: 'Card Placeholder', image: '../../../../assets/images/Hand_Made_Silk_Cushions.jpg' },
+    { id: 5, title: 'Card Placeholder', image: '../../../../assets/images/Shea_Butter_Body_Cream.jpg' },
+    { id: 6, title: 'Card Placeholder', image: '../../../../assets/images/Hand_Knitted_Socks.jpg' },
+    { id: 7, title: 'Card Placeholder', image: '../../../../assets/images/Hand_Made_Silk_Cushions.jpg' },
+    { id: 8, title: 'Card Placeholder', image: '../../../../assets/images/Hand_Knitted_Socks.jpg' },
+  ]
 }
