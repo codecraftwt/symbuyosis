9b937a7c82eafc345bdc8dbdc0cbb8961b83020b
codecraftwt <codecraftwt@gmail.com>
2025-04-28 19:32:05 +0530

    login page added and create-account page updated

--- src/app/auth/components/create-account/create-account.component.ts
+++ src/app/auth/components/create-account/create-account.component.ts
@@ -1,6 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+
+export const passwordMatchValidator: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
+  const password = control.get('password');
+  const confirmPassword = control.get('confirmPassword');
+
+  return password && confirmPassword && password.value !== confirmPassword.value 
+    ? { passwordMismatch: true }
+    : null;
+};
 
 @Component({
   selector: 'app-create-account',
@@ -20,7 +29,7 @@ export class CreateAccountComponent implements OnInit {
   }
 
   protected onSubmit() {
-    this._router.navigate(['layout/']);
+    // this._router.navigate(['layout/']);
     if (this.accountForm.valid) {
       console.log('Form submitted:', this.accountForm.value);
       // Add your form submission logic here
@@ -43,6 +52,12 @@ export class CreateAccountComponent implements OnInit {
   get email(): FormControl {
     return this.accountForm.get('email') as FormControl;
   }
+  get password(): FormControl {
+    return this.accountForm.get('password') as FormControl;
+  }
+  get confirmPassword(): FormControl {
+    return this.accountForm.get('confirmPassword') as FormControl;
+  }
   get termsAndConditions(): FormControl {
     return this.accountForm.get('termsAndConditions') as FormControl;
   }
@@ -51,8 +66,20 @@ export class CreateAccountComponent implements OnInit {
     this.accountForm = this._fb.group({
       name: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      confirmPassword: ['', Validators.required],
       termsAndConditions: [false, Validators.requiredTrue],
-    })
+    }, { validators: passwordMatchValidator })
   }
 
+  protected checkPassword() {
+    const password = this.accountForm.get('password');
+    const confirmPassword = this.accountForm.get('confirmPassword');
+
+    if (password?.value !== confirmPassword?.value) {
+      confirmPassword?.setErrors({ passwordMismatch: true });
+    } else {
+      confirmPassword?.setErrors(null);
+    }
+  }
 }
--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,12 +1,56 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  public accountForm!: FormGroup;
 
+  constructor(private _fb: FormBuilder, private _router: Router) { }
+
+  ngOnInit(): void {
+    this.initilizeForm();
+  }
+
+  protected onLogin() {
+    // this._router.navigate(['layout/']);
+    if (this.accountForm.valid) {
+      console.log('Form submitted:', this.accountForm.value);
+      // Add your form submission logic here
+    } else {
+      // Mark all fields as touched to trigger validation messages
+      Object.keys(this.accountForm.controls).forEach(key => {
+        const control = this.accountForm.get(key);
+        control?.markAsTouched();
+      });
+    }
+  }
+
+  protected navigateToCreateAccount() {
+    this._router.navigate(['create-account']);
+  }
+
+  get email(): FormControl {
+    return this.accountForm.get('email') as FormControl;
+  }
+  get password(): FormControl {
+    return this.accountForm.get('password') as FormControl;
+  }
+  get termsAndConditions(): FormControl {
+    return this.accountForm.get('termsAndConditions') as FormControl;
+  }
+
+  private initilizeForm() {
+    this.accountForm = this._fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.email]],
+      termsAndConditions: [false, Validators.requiredTrue],
+    })
+  }
 }
