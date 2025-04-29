74fc86beb3a60e15c0c0ff9fecd75a7b3191e7df
codecraftwt <codecraftwt@gmail.com>
2025-04-29 20:01:22 +0530

    first header bigger others smaller in promo

--- src/app/shared/components/promo-panel/promo-panel.component.ts
+++ src/app/shared/components/promo-panel/promo-panel.component.ts
@@ -1,14 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-promo-panel',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './promo-panel.component.html',
   styleUrl: './promo-panel.component.scss'
 })
 export class PromoPanelComponent {
   @Input() public heading: string = '';
+  @Input() public isFirstheading: boolean = false;
   @Input() public description: string = '';
   @Input() public buttonText: string = '';
 }
